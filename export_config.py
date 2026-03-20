b3788576a5f0e974251004383dff2b4f606bd10a
梅雨 <13770848194@163.com>
2026-03-20 17:53:17 +0800

    dev

--- export_config.py
+++ export_config.py
@@ -1,6 +1,6 @@
 
 from pathlib import Path
-model_home = Path('~/.cache/modelscope/hub/models/Qwen').expanduser()
+model_home = Path('./models').expanduser()
 
 
 # [源模型路径] 官方下载好的 SafeTensors 模型文件夹
@@ -8,4 +8,4 @@ ASR_MODEL_DIR =  model_home / 'Qwen3-ASR-1.7B'
 ALIGNER_MODEL_DIR =  model_home / 'Qwen3-ForcedAligner-0.6B'
 
 # [导出目标路径] 转换后的 ONNX, GGUF 和权重汇总目录
-EXPORT_DIR = r'./model'
+EXPORT_DIR = r'model'
